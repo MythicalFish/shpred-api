0cf7f835f6fa9359ef909279ce0b6fadc947a95d
jake <jake@mythical.fish>
2017-12-17 18:16:56 +0000

    add show action to api

--- app/controllers/videos_controller.rb
+++ app/controllers/videos_controller.rb
@@ -1,6 +1,20 @@
 class VideosController < ApiController
+
+  before_action :set_video, except: :index
+
   def index
     @videos = Video.exposed
     render json: @videos, each_serializer: VideoListSerializer
   end
+
+  def show
+    render json: @video
+  end
+
+  private
+
+  def set_video
+    @video = Video.find(params[:id])
+  end
+
 end
